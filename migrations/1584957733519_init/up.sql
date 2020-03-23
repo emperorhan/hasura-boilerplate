3e8a9e0cd00822bb5feec44e7f941240b3aa8fe3
emperorhan <iny27@naver.com>
2020-03-23 19:03:31 +0900

    feat: init

--- migrations/1584957733519_init/up.sql
+++ migrations/1584957733519_init/up.sql
@@ -20,10 +20,10 @@ CREATE TABLE public.enum_user_role (
 );
 CREATE TABLE public.task_history (
     id bigint NOT NULL,
-    "taskId" integer NOT NULL,
-    "userId" integer NOT NULL,
-    "historyType" text NOT NULL,
-    "columnName" text NOT NULL,
+    task_id integer NOT NULL,
+    user_id integer NOT NULL,
+    history_type text NOT NULL,
+    column_name text NOT NULL,
     before text NOT NULL,
     after text NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
@@ -52,25 +52,25 @@ CREATE SEQUENCE public.tasks_id_seq
     CACHE 1;
 ALTER SEQUENCE public.tasks_id_seq OWNED BY public.tasks.id;
 CREATE TABLE public.user_roles (
-    "userId" integer NOT NULL,
+    user_id integer NOT NULL,
     role text NOT NULL
 );
 CREATE TABLE public.user_tasks (
-    "userId" integer NOT NULL,
-    "taskId" integer NOT NULL,
-    comment text NOT NULL
+    user_id integer NOT NULL,
+    task_id integer NOT NULL,
+    comment text
 );
 CREATE TABLE public.users (
     id integer NOT NULL,
     uuid uuid NOT NULL,
-    "firstName" text NOT NULL,
-    "lastName" text NOT NULL,
-    "accountName" text NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    account_name text NOT NULL,
     password text NOT NULL,
+    salt text NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL,
     updated_at timestamp with time zone DEFAULT now() NOT NULL,
-    deleted_at timestamp with time zone,
-    salt text NOT NULL
+    deleted_at timestamp with time zone
 );
 CREATE SEQUENCE public.users_id_seq
     AS integer
@@ -85,22 +85,18 @@ ALTER TABLE ONLY public.tasks ALTER COLUMN id SET DEFAULT nextval('public.tasks_
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
 ALTER TABLE ONLY public.enum_history_type
     ADD CONSTRAINT enum_history_type_pkey PRIMARY KEY (type);
-ALTER TABLE ONLY public.enum_history_type
-    ADD CONSTRAINT enum_history_type_value_key UNIQUE (type);
 ALTER TABLE ONLY public.enum_user_role
-    ADD CONSTRAINT enum_user_role_value_key UNIQUE (role);
-ALTER TABLE ONLY public.enum_user_role
-    ADD CONSTRAINT enum_user_roles_pkey PRIMARY KEY (role);
+    ADD CONSTRAINT enum_user_role_pkey PRIMARY KEY (role);
 ALTER TABLE ONLY public.task_history
     ADD CONSTRAINT task_history_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.tasks
     ADD CONSTRAINT tasks_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.user_roles
-    ADD CONSTRAINT user_roles_pkey PRIMARY KEY ("userId", role);
+    ADD CONSTRAINT user_roles_pkey PRIMARY KEY (user_id, role);
 ALTER TABLE ONLY public.user_tasks
-    ADD CONSTRAINT user_tasks_pkey PRIMARY KEY ("userId", "taskId");
+    ADD CONSTRAINT user_tasks_pkey PRIMARY KEY (user_id, task_id);
 ALTER TABLE ONLY public.users
-    ADD CONSTRAINT "users_accountName_key" UNIQUE ("accountName");
+    ADD CONSTRAINT users_account_name_key UNIQUE (account_name);
 ALTER TABLE ONLY public.users
     ADD CONSTRAINT users_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.users
@@ -110,19 +106,19 @@ COMMENT ON TRIGGER set_public_tasks_updated_at ON public.tasks IS 'trigger to se
 CREATE TRIGGER set_public_users_updated_at BEFORE UPDATE ON public.users FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_users_updated_at ON public.users IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 ALTER TABLE ONLY public.task_history
-    ADD CONSTRAINT "task_history_historyType_fkey" FOREIGN KEY ("historyType") REFERENCES public.enum_history_type(type) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT task_history_history_type_fkey FOREIGN KEY (history_type) REFERENCES public.enum_history_type(type) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.task_history
-    ADD CONSTRAINT "task_history_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES public.tasks(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT task_history_task_id_fkey FOREIGN KEY (task_id) REFERENCES public.tasks(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.task_history
-    ADD CONSTRAINT "task_history_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT task_history_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.user_roles
     ADD CONSTRAINT user_roles_role_fkey FOREIGN KEY (role) REFERENCES public.enum_user_role(role) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.user_roles
-    ADD CONSTRAINT "user_roles_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT user_roles_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.user_tasks
-    ADD CONSTRAINT "user_tasks_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES public.tasks(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT user_tasks_task_id_fkey FOREIGN KEY (task_id) REFERENCES public.tasks(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 ALTER TABLE ONLY public.user_tasks
-    ADD CONSTRAINT "user_tasks_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+    ADD CONSTRAINT user_tasks_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE RESTRICT ON DELETE RESTRICT;
 
 -- ENUM data
 INSERT INTO public.enum_history_type (type, comment) VALUES ('CREATION', '생성');
@@ -131,3 +127,7 @@ INSERT INTO public.enum_history_type (type, comment) VALUES ('DELETION', '삭제
 INSERT INTO public.enum_user_role (role, comment) VALUES ('USER', '사용자 권한');
 INSERT INTO public.enum_user_role (role, comment) VALUES ('ADMIN', '시스템 관리자 권한');
 INSERT INTO public.enum_user_role (role, comment) VALUES ('ANONYMOUS', '비회원 권한');
+-- SEQ data
+SELECT pg_catalog.setval('public.task_history_id_seq', 1, false);
+SELECT pg_catalog.setval('public.tasks_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_id_seq', 1, false);
